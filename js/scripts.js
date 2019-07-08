c4cb11a92ce33c754ab3a67d684054fe02d2201f
Nicolás Patarino <npatarino@gmail.com>
2019-07-09 00:12:10 +0200

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -213,6 +213,7 @@ $(document).ready(function () {
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
             $.post('https://script.google.com/macros/s/AKfycbzoQFx5LlPT6VHKiPZhl2i6cPGpCjdt92K6ttCSf4ZP2nsxj_4f/exec', data)
                 .done(function (data) {
+                    console.log("Patarino");
                     console.log(data);
                     $('#alert-wrapper').html('');
                     $('#rsvp-modal').modal('show');
